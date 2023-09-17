510d2cdc19c5b2168346302f635770c0d5049f10
Yinnohs <joistleon@gmail.com>
2023-09-17 23:52:50 +0100

    :sparkles:(main): added logo component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import { Container , Box, Heading} from '@chakra-ui/react'
+
 const Page = () => {
-  return <h1> hi </h1>
+  return (
+    <Container>
+        <Box borderRadius={'md'} bg={'red.500'} mb={5} p={2.5} textAlign={'center'}>
+            Hi, I'm a fullstack developer based on Spain!
+        </Box>
+        <Box display={{md:'flex'}}>
+        <Box flexGrow={1}></Box>
+        <Heading as="h2" variant={"page-tittle"}>
+            Jose Ignacio Soto Leon
+        </Heading>
+        <Heading as="h3" variant={"page-tittle"}>
+            yinnohs
+        </Heading>
+        <p>Backend oriented fullstack Developer</p>
+        </Box>
+    </Container>
+  )
 }
 
 export default Page
